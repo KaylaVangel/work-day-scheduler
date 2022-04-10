cc858831ca9187d2f51dbf9b300339a8a89e25c9
Kayla Vangel <Kayla.Vangel@comcast.net>
2022-04-10 11:42:06 -0400

    fixed classes on textarea

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -1,24 +1,32 @@
 
 
 
-setInterval(checkTime,1000);
+setInterval(checkTime, 1000,);
 function checkTime() {
     var dateToDisplay = moment();
-   document.getElementById("currentDay").innerHTML= dateToDisplay;
-
-//    if (time < hour) {
-
-
-//    }
-}
+    document.getElementById("currentDay").innerHTML = dateToDisplay;
+    let timeBlocks = Array.from(document.getElementsByClassName("time-block"));
 
 
+    for (let i = 9; i < (9 + timeBlocks.length); i++) {
+        if (i < moment().format("H")) {
+            timeBlocks[i - 9].querySelector("textarea").setAttribute("class", "past col-md-10 description");
+        }
+        if (i == moment().format("H")) {
+            timeBlocks[i - 9].querySelector("textarea").setAttribute("class", "present col-md-10 description");
+        }
+        if (i > moment().format("H")) {
+            timeBlocks[i - 9].querySelector("textarea").setAttribute("class", "future col-md-10 description");
+        }
+
+    };
+
+    function save(button) {
+        let saveText = button.previousElementSibling.value;
+        let hour = button.parentElement.id
+        localStorage.setItem(hour, saveText);
+        console.log(button.parentElement.id);
  
-function save(button) {
-    let saveText = button.previousElementSibling.value;
-    let hour = button.parentElement.id
-    localStorage.setItem(hour, saveText);
- console.log(button.parentElement.id);
-
+   };
 
 };
